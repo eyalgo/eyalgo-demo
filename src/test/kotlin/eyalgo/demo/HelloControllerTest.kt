2697769bd3cca4f2168832a7e1ae70f9ef1ab26f
Eyal Golan <eyal.golan@signavio.com>
2024-02-20 00:11:52 +0100

    add `FlywayEndpointIT`. Need to check the serialisation problem

--- src/test/kotlin/eyalgo/demo/HelloControllerTest.kt
+++ src/test/kotlin/eyalgo/demo/HelloControllerTest.kt
@@ -1,15 +1,13 @@
 package eyalgo.demo
 
 import eyalgo.demo.matchers.Is
-import io.micronaut.http.client.HttpClient
-import io.micronaut.http.client.annotation.Client
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import io.restassured.module.kotlin.extensions.Then
 import io.restassured.module.kotlin.extensions.When
 import org.junit.jupiter.api.Test
 
 @MicronautTest
-class HelloControllerTest(@field:Client("/") val client: HttpClient) {
+class HelloControllerTest {
 
     @Test
     fun helloWorld1() {
--- src/test/kotlin/eyalgo/demo/infrastructure/FlywayEndpointIT.kt
+++ src/test/kotlin/eyalgo/demo/infrastructure/FlywayEndpointIT.kt
@@ -1,10 +1,23 @@
 package eyalgo.demo.infrastructure
 
+import io.micronaut.http.HttpStatus.OK
 import io.micronaut.http.client.HttpClient
 import io.micronaut.http.client.annotation.Client
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import io.restassured.module.kotlin.extensions.Then
+import io.restassured.module.kotlin.extensions.When
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
 
 @MicronautTest
 class FlywayEndpointIT(@field:Client("/") val client: HttpClient) {
-
+    @Test
+    @Disabled("There are issues with the serialisation of the flyway endpoint")
+    fun `verify flyway endpoint`() {
+        When {
+            get("/flyway")
+        } Then {
+            statusCode(OK.code)
+        }
+    }
 }
